bd83fe9092b7e36948db15ec47d33dc8f64c87a9
Woensug Choi <woensug.choi@gmail.com>
2022-02-15 13:53:21 +0900

    More smoothing and fixes for SDF for single tile

--- mkbathy.py
+++ mkbathy.py
@@ -14,18 +14,31 @@ from tqdm import trange
 ###################################################################
 PREFIX = 'MontereyBay'
 SOURCE = 'bathymetry_source/monterey_13_navd88_2012.nc'
+
+# Flag for single tile generation
+ONE_TILE_AT_ORIGIN = False
+# Flag for more smoothness
+SMOOTH_MORE = False
+
 # Range
 STARTLON = -121.825
 STARTLAT = 36.790
 ENDLON = -121.805
 ENDLAT = 36.810
+
 # Resolution
 DLON = 0.01
 DLAT = 0.01
 OVERLON = 0.0005
 OVERLAT = 0.0005
 ###################################################################
+
 TEMPLATE_DIR = '/Bathymetry_Converter/'
+if ONE_TILE_AT_ORIGIN:
+    DLON = ENDLON-STARTLON
+    DLAT = ENDLAT-STARTLAT
+    OVERLON = 0.0
+    OVERLAT = 0.0
 
 print("\n")
 print("#--------------------------------------------------------#")
@@ -35,13 +48,28 @@ print("# Prefix : " + PREFIX)
 print("# Source : " + SOURCE)
 print("# Range -- Latitude (N) : " + repr(STARTLAT) + " ~ " + repr(ENDLAT))
 print("#       \_ longitude(E) : " + repr(STARTLON) + " ~ " + repr(ENDLON))
-print("# Resolution -- Latitude  : " + repr(DLAT) + " with " + repr(OVERLAT) + " overlaps")
-print("#            \_ longitude : " + repr(DLON) + " with " + repr(OVERLON) + " overlaps")
+if ONE_TILE_AT_ORIGIN:
+    print("# ONE_TILE_AT_ORIGIN flag is ON! Size (Lat, Lon) = (" + repr(DLAT) + ", " + repr(DLON) + ")")
+else:
+    print("# Resolution -- Latitude  : " + repr(DLAT) + " with " + repr(OVERLAT) + " overlaps")
+    print("#            \_ longitude : " + repr(DLON) + " with " + repr(OVERLON) + " overlaps")
 print("#--------------------------------------------------------#\n")
 
 nLon = int(math.ceil((ENDLON-OVERLON-STARTLON)/DLON))
 nLat = int(math.ceil((ENDLAT-OVERLAT-STARTLAT)/DLAT))
 nTot = nLon * nLat
+
+# Get No Data Value (to skip later)
+source = gdal.Open(SOURCE)
+band = source.GetRasterBand(1)
+band.GetStatistics(True, True)
+maxHeight = band.GetMaximum()
+NoDataValue = band.GetNoDataValue()
+print('Source statistics : ')
+print('  Maximum Height : ' + repr(band.GetMaximum()))
+print('  Minimum Height : ' + repr(band.GetMinimum()))
+print('  NoDataValue represented as : ' + repr(band.GetNoDataValue()))
+print(' ')
 
 # Make bathymetry directory (if not exist)
 Path('bathymetry').mkdir(parents=True, exist_ok=True) # Template directory
@@ -49,15 +77,12 @@ Path(PREFIX).mkdir(parents=True, exist_ok=True) # Final product directory
 PATH = "bathymetry/"
 
 # Reproject into WGS (EPSG:4326)
-print("Initiating... (It may take longer to initiate for large source")
+print("Initiating... (It may take longer to initiate for large source)")
 os.system("PROJ_NETWORK=ON gdalwarp -t_srs 'EPSG:4326' -of GMT " \
           + SOURCE + " " + PATH + PREFIX + '.grd')
 
-# Get No Data Value (to skip later)
-source = gdal.Open(SOURCE)
-band = source.GetRasterBand(1)
-NoDataValue = band.GetNoDataValue()
-
+print(" ")
+print("Starting conversion..")
 longitude = STARTLON
 latitude = STARTLAT
 # Loop longitude
@@ -83,6 +108,8 @@ for lon in trange(nLon):
         # Cut and translate into XYZ point cloud (verbose error only)
         xyz = pygmt.grd2xyz(PATH + PREFIX + '.grd', output_type='numpy', \
              verbose='e', region=[west_edge, east_edge, south_edge, north_edge])
+        center_lon = (west_edge + east_edge)/2.0
+        center_lat = (south_edge + north_edge)/2.0
 
         # Transform project coordniate system from WGS(EPSG:4326) to UTM(EPSG:3857)
         source = osr.SpatialReference(); source.ImportFromEPSG(4326)
@@ -94,8 +121,19 @@ for lon in trange(nLon):
             point = ogr.CreateGeometryFromWkt( \
                     "POINT (" + repr(pt[1]) + " " + repr(pt[0]) + ")")
             point.Transform(transform)
-            pt[0] = point.ExportToWkt().split(" ")[1].split("(")[1]
-            pt[1] = point.ExportToWkt().split(" ")[2].split(")")[0]
+            if ONE_TILE_AT_ORIGIN:
+                center = ogr.CreateGeometryFromWkt( \
+                        "POINT (" + repr(center_lat) + " " + repr(center_lon) + ")")
+                center.Transform(transform)
+                pt[0] = float(point.ExportToWkt().split(" ")[1].split("(")[1]) \
+                        - float(center.ExportToWkt().split(" ")[1].split("(")[1])
+                pt[1] = float(point.ExportToWkt().split(" ")[2].split(")")[0]) \
+                        - float(center.ExportToWkt().split(" ")[2].split(")")[0])
+                # pt[2] = float(pt[2]) - float(maxHeight)
+            else:
+                pt[0] = point.ExportToWkt().split(" ")[1].split("(")[1]
+                pt[1] = point.ExportToWkt().split(" ")[2].split(")")[0]
+
 
         # Save asc file with 'X Y Z' header for meshlab
         filename = PATH + PREFIX + "." + tileName + '.epsg3857'
@@ -111,10 +149,9 @@ for lon in trange(nLon):
         np.savetxt(filename + '.asc', xyz[index], fmt='%10.5f', delimiter=' ')
 
         # Make texture using colormap
-        gdal.Translate(filename + '.tif', filename + '.asc', \
-              options='-of GTiff -a_srs EPSG:3857')
+        gdal.Translate(filename + '.tif', filename + '.asc', options='-of GTiff -a_srs EPSG:3857')
         gdal.DEMProcessing(filename + '.color.tif', filename + '.tif', "color-relief", \
-              colorFilename=TEMPLATE_DIR+'color.txt')
+              colorFilename='color.txt')
         texture = gdal.Translate(filename + '.texture.png', filename + '.color.tif', \
               options='-of PNG -ot UInt16 -scale 32.53501 767.4913 0 65535')
 
@@ -122,6 +159,8 @@ for lon in trange(nLon):
         ms = ml.MeshSet()
         ms.load_new_mesh(filename + '.meshlab.asc', triangulate=True)
         ms.apply_filter('taubin_smooth')
+        if SMOOTH_MORE:
+            ms.apply_filter('twostep_smooth', normalthr=30.0, stepsmoothnum=5)
         ms.apply_filter('invert_faces_orientation')
         ms.apply_filter('parametrization_flat_plane', projectionplane='XY')
         ms.save_current_mesh(filename + '.obj')
@@ -130,28 +169,46 @@ for lon in trange(nLon):
         # Construct model object
         TEMPLATE_PATH = TEMPLATE_DIR + 'templates'
         MODEL_URI = PREFIX + "." + tileName + '.epsg3857'
-        MODEL_DIR = filename
+        if ONE_TILE_AT_ORIGIN:
+            MODEL_DIR = PREFIX
+        else:
+            MODEL_DIR = filename
         Path(MODEL_DIR + "/mesh").mkdir(parents=True, exist_ok=True)
-        os.system("cp " + MODEL_DIR + ".obj " + MODEL_DIR + "/mesh/" )
-        os.system("cp " + MODEL_DIR + ".obj.mtl " + MODEL_DIR + "/mesh/" )
+        os.system("cp " + filename + ".obj " + MODEL_DIR + "/mesh/" )
+        os.system("cp " + filename + ".obj.mtl " + MODEL_DIR + "/mesh/" )
 
         # create model.config
-        os.system("cat " + TEMPLATE_PATH + "/model.config | sed s#MODEL_NAME#" + PREFIX + "." + tileName + "#g > " + MODEL_DIR + "/model.config")
+        if ONE_TILE_AT_ORIGIN:
+            os.system("cat " + TEMPLATE_PATH + "/model.config | sed s#MODEL_NAME#" + PREFIX + "#g > " + MODEL_DIR + "/model.config")
+        else:
+            os.system("cat " + TEMPLATE_PATH + "/model.config | sed s#MODEL_NAME#" + PREFIX + "." + tileName + "#g > " + MODEL_DIR + "/model.config")
 
         # create model.sdf
         os.system("cat " + TEMPLATE_PATH + "/model.sdf > " + MODEL_DIR +"/model.sdf")
-        os.system("sed -i s#MODEL_NAME#" + PREFIX + "." + tileName + "#g " + MODEL_DIR + "/model.sdf")
-        os.system("sed -i s#MODEL_URI#model://" + MODEL_URI + "/mesh/" + MODEL_URI + ".obj#g " + MODEL_DIR + "/model.sdf")
+        if ONE_TILE_AT_ORIGIN:
+            os.system("sed -i s#MODEL_NAME#" + PREFIX + "#g " + MODEL_DIR + "/model.sdf")
+            os.system("sed -i s#MODEL_URI#model://" + PREFIX + "/mesh/" + MODEL_URI + ".obj#g " + MODEL_DIR + "/model.sdf")
+        else:
+            os.system("sed -i s#MODEL_NAME#" + PREFIX + "." + tileName + "#g " + MODEL_DIR + "/model.sdf")
+            os.system("sed -i s#MODEL_URI#model://" + MODEL_URI + "/mesh/" + MODEL_URI + ".obj#g " + MODEL_DIR + "/model.sdf")
 
         # Create material
         os.system("cp -r " + TEMPLATE_PATH + "/materials " + MODEL_DIR + "/.")
-        os.system("cp " + MODEL_DIR + ".texture.png " + MODEL_DIR + "/materials/textures/.")
-        os.system("sed -i s#MODEL_NAME#" + PREFIX + "." + tileName + "#g " + MODEL_DIR + "/materials/scripts/texture.material")
+        os.system("cp " + filename + ".texture.png " + MODEL_DIR + "/materials/textures/.")
         os.system("sed -i s#TEXTURE_NAME#" + MODEL_URI + ".texture.png#g " + MODEL_DIR + "/materials/scripts/texture.material")
-        os.system("sed -i s#TEXTURE_URI#model://" + MODEL_URI + "#g " + MODEL_DIR + "/model.sdf")
+
+        if ONE_TILE_AT_ORIGIN:
+            os.system("sed -i s#MODEL_NAME#" + PREFIX + "#g " + MODEL_DIR + "/materials/scripts/texture.material")
+            os.system("sed -i s#TEXTURE_URI#model://" + PREFIX + "#g " + MODEL_DIR + "/model.sdf")
+        else:
+            os.system("sed -i s#MODEL_NAME#" + PREFIX + "." + tileName + "#g " + MODEL_DIR + "/materials/scripts/texture.material")
+            os.system("sed -i s#TEXTURE_URI#model://" + MODEL_URI + "#g " + MODEL_DIR + "/model.sdf")
 
         # Relocate final product
-        os.system("cp -r " + MODEL_DIR + " " + PREFIX + "/.")
+        if ONE_TILE_AT_ORIGIN:
+            os.system("cp -r " + MODEL_DIR + "/* " + PREFIX + "/.")
+        else:
+            os.system("cp -r " + MODEL_DIR + " " + PREFIX + "/.")
 
         # update latitude
         latitude = north
@@ -162,6 +219,11 @@ for lon in trange(nLon):
 
 # Remove template directory
 os.system("rm -r bathymetry")
+
+# Print generation memo
+if ONE_TILE_AT_ORIGIN:
+    print(" Important memo : ")
+    print("   A tile is lifted up for " + repr(float(maxHeight)) + " from the original height")
 
 # DONE!
 print("\n Done.\n")
